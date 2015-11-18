b391f92da69373ce54421cbd43473bb62f259701
jeffrey <jeffcho59@gmail.com>
2015-11-17 19:54:41 -0500

    added web request files

--- webrequest/ViewController.swift
+++ webrequest/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        container.addSubview(webView)
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+    let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+    webView.frame = frame
+        
+    
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func loadRequest (urlStr: String) {
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
+    @IBAction func loadSwift(sender: AnyObject) {
+        loadRequest("https://developer.apple.com/swift/blog/")
+    }
 
-
+    @IBAction func loadSteak(sender: AnyObject) {
+        loadRequest("http://www.steak-enthusiast.com")
+    }
+    
+    @IBAction func loadGossip(sender: AnyObject) {
+        loadRequest("http://www.tmz.com")
+    }
+    
 }
 
